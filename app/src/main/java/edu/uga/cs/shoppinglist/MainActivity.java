669ad79895d51ea22b45494eb56853c8bbd6e731
EdwardYoun <ey26416@uga.edu>
2023-04-19 01:55:29 -0400

    splash screen design

--- app/src/main/java/edu/uga/cs/shoppinglist/MainActivity.java
+++ app/src/main/java/edu/uga/cs/shoppinglist/MainActivity.java
@@ -2,7 +2,16 @@ package edu.uga.cs.shoppinglist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +19,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test
+
+        Button register = findViewById(R.id.button2);
+        Button login = findViewById(R.id.button3);
+
+        register.setOnClickListener( new RegisterButtonClickListener() );
+        login.setOnClickListener( new LoginButtonClickListener() );
+    }
+
+    private class RegisterButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick( View v ) {
+
+        }
+    }
+
+    private class LoginButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick( View v ) {
+
+        }
     }
 }
