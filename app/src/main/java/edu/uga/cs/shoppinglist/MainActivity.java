a8b3acd2b44d3ac03ff959ca8cf20d4a1d79ff58
EdwardYoun <82770744+EdwardYoun@users.noreply.github.com>
2023-04-21 19:00:40 -0400

    login inserted

--- app/src/main/java/edu/uga/cs/shoppinglist/MainActivity.java
+++ app/src/main/java/edu/uga/cs/shoppinglist/MainActivity.java
@@ -1,5 +1,7 @@
 package edu.uga.cs.shoppinglist;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,13 +9,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String DEBUG_TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +46,63 @@ public class MainActivity extends AppCompatActivity {
     private class LoginButtonClickListener implements View.OnClickListener {
         @Override
         public void onClick( View v ) {
-            Intent intent = new Intent(v.getContext(), LoginActivity.class);
-            v.getContext().startActivity(intent);
+            // This is an example of how to use the AuthUI activity for signing in to Firebase.
+            // Here, we are just using email/password sign in.
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.EmailBuilder().build()
+            );
+
+            Log.d( DEBUG_TAG, "MainActivity.SignInButtonClickListener: Signing in started" );
+
+            // Create an Intent to singin to Firebese.
+            Intent signInIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build();
+            signInLauncher.launch(signInIntent);
+        }
+    }
+
+    // The ActivityResultLauncher class provides a new way to invoke an activity
+    // for some result.  It is a replacement for the deprecated method startActivityForResult.
+    //
+    // The signInLauncher variable is a launcher to start the AuthUI's logging in process that
+    // should return to the MainActivity when completed.  The overridden onActivityResult
+    // is then called when the Firebase logging-in process is finished.
+    private ActivityResultLauncher<Intent> signInLauncher =
+            registerForActivityResult(
+                    new FirebaseAuthUIActivityResultContract(),
+                    new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                        @Override
+                        public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                            onSignInResult(result);
+                        }
+                    }
+            );
+
+    // This method is called once the Firebase sign-in activity (lqunched above) returns (completes).
+    // Then, the current (logged-in) Firebase user can be obtained.
+    // Subsequently, there is a transition to the JobLeadManagementActivity.
+    private void onSignInResult( FirebaseAuthUIAuthenticationResult result ) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            if( response != null ) {
+                Log.d( DEBUG_TAG, "MainActivity.onSignInResult: response.getEmail(): " + response.getEmail() );
+            }
+
+            //Log.d( DEBUG_TAG, "MainActivity.onSignInResult: Signed in as: " + user.getEmail() );
+
+            // after a successful sign in, start the job leads management activity
+            Intent intent = new Intent( this, UserActivity.class );
+            startActivity( intent );
+        }
+        else {
+            Log.d( DEBUG_TAG, "MainActivity.onSignInResult: Failed to sign in" );
+            // Sign in failed. If response is null the user canceled the
+            Toast.makeText( getApplicationContext(),
+                    "Sign in failed",
+                    Toast.LENGTH_SHORT).show();
         }
     }
 }
