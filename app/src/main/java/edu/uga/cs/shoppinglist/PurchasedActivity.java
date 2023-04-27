4ac03274501b0b368196861507426ae25ed1358f
pennogilvie <wpo92700@uga.edu>
2023-04-27 17:52:12 -0400

    Updated layout for PurchasedActivity

--- app/src/main/java/edu/uga/cs/shoppinglist/PurchasedActivity.java
+++ app/src/main/java/edu/uga/cs/shoppinglist/PurchasedActivity.java
@@ -4,11 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -20,49 +16,29 @@ import java.util.ArrayList;
 
 public class PurchasedActivity extends AppCompatActivity {
 
-    private ArrayList<Purchased> purchasedList;
-    private ListView purchasedView;
-    private PurchasedAdapter purchasedAdapter;
+    private ListView purchasedList;
     private DatabaseReference purchasedReference;
-    private TextView itemsView, userView;
-    private EditText totalView;
-    private Button priceButton, editButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purchased);
         purchasedReference = FirebaseDatabase.getInstance().getReference("purchased");
-        purchasedView = findViewById(R.id.purchasedItems);
-        itemsView = findViewById(R.id.textView7);
-        totalView = findViewById(R.id.textView8);
-        userView = findViewById(R.id.textView9);
-        priceButton = findViewById(R.id.button14);
-        editButton = findViewById(R.id.button15);
-
-        itemsView.setVisibility(View.GONE);
-        totalView.setVisibility(View.GONE);
-        userView.setVisibility(View.GONE);
-        priceButton.setVisibility(View.GONE);
-        editButton.setVisibility(View.GONE);
-
-        purchasedList = new ArrayList<>();
-        purchasedAdapter = new PurchasedAdapter(this, purchasedList);
-        purchasedView.setAdapter(purchasedAdapter);
-
-        purchasedReference.addValueEventListener(new ValueEventListener() {
+        purchasedList = findViewById(R.id.purchasedItems);
+        purchasedReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                purchasedList.clear();
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    Purchased item = snapshot.getValue(Purchased.class);
-                    purchasedList.add(item);
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ArrayList<Item> itemList = new ArrayList<>();
+                for (DataSnapshot itemSnapshot : snapshot.getChildren()) {
+                    Item item = itemSnapshot.getValue(Item.class);
+                    itemList.add(item);
                 }
-                purchasedAdapter.notifyDataSetChanged();
+
             }
+
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                // Handle errors here
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
